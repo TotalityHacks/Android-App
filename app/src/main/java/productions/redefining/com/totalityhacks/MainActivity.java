9a9b8cb805fab4255ebf7847d9759d4b3847ba81
mike2151 <redefiningproductions@gmail.com>
2018-05-18 16:11:58 -0400

    init

---
+++ app/src/main/java/productions/redefining/com/totalityhacks/MainActivity.java
@@ -0,0 +1,41 @@
+package productions.redefining.com.totalityhacks;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_schedule:
+
+                            case R.id.action_event:
+
+                            case R.id.action_check_in:
+
+                            case R.id.action_announcements:
+
+                            case R.id.action_maps:
+
+                        }
+                        return true;
+                    }
+                });
+
+
+    }
+}
