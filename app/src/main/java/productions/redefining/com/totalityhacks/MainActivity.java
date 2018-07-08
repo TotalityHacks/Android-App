2342a7b7aeb4bf6ae5fd3c14d754bcef0054aa1a
mike2151 <redefiningproductions@gmail.com>
2018-07-07 22:22:21 -0700

    got nav bar working

--- app/src/main/java/productions/redefining/com/totalityhacks/MainActivity.java
+++ app/src/main/java/productions/redefining/com/totalityhacks/MainActivity.java
@@ -1,21 +1,44 @@
 package productions.redefining.com.totalityhacks;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+        bottomNavigationView = (BottomNavigationView)
                 findViewById(R.id.bottom_navigation);
+        initNavBar(bottomNavigationView);
 
+    }
+
+
+    //common functions for all activities
+
+    public void initNavBar(BottomNavigationView bottomNavigationView) {
+        bottomNavigationView.setItemIconTintList(null);
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -35,7 +58,5 @@ public class MainActivity extends AppCompatActivity {
                         return true;
                     }
                 });
-
-
     }
 }
